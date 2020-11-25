ce7987f196a200965e65828478fd4c25cbf6c243
can aktürk <52425556+twichorg@users.noreply.github.com>
2020-11-25 18:40:45 +0300

    redux updated

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import {store, persistor} from './redux/store';
+import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
@@ -12,13 +12,10 @@ import App from './App';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-    <PersistGate persistor={persistor}>
-       <App />
+      <PersistGate persistor={persistor}>
+        <App />
       </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
-
-
-
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import {persistStore} from 'redux-persist';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
@@ -8,6 +8,6 @@ const middlewares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
 
-export default {store, persistor};
+export default { store, persistStore };
